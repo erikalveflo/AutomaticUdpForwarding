221bc27d371bb47f8d75d2891ebaab9b40ae2bd3
Erik Alveflo <erikalveflo@users.noreply.github.com>
2024-04-20 16:38:36 +0200

    Removes unused usings

--- CSharpExample/Program.cs
+++ CSharpExample/Program.cs
@@ -1,19 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
-using System.Net.NetworkInformation;
-using System.Threading;
-using System.Runtime.Remoting.Messaging;
-using System.ComponentModel;
-using System.Net.Http.Headers;
-using System.Data;
-using System.Security;
-using System.Text.RegularExpressions;
-using System.Runtime.InteropServices;
 
 namespace CSharpExample
 {
--- Emulators/F1_2017/Program.cs
+++ Emulators/F1_2017/Program.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Sockets;
 using System.Net;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace F1_2017
 {
